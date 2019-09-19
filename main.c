41cfa2ed50c6c19e723dabb7722296c5530eaf30
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2019-09-19 15:32:41 +0900

    3week practice

--- main.c
+++ main.c
@@ -5,7 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!\n");
+	int x;
+	char y;
+	short z;
+	float m;
+	double n;
+	
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of char : %d\n", sizeof(y));
+	printf("bytes of short : %d\n", sizeof(z));
+	printf("bytes of float : %d\n", sizeof(m));
+	printf("bytes of double : %d\n", sizeof(n));
 	
 	return 0;
 }
