5e56e4fd8abffa93a231b9c47c1abe8eafd861d2
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2019-09-19 16:04:08 +0900

    3week practice2

--- main.c
+++ main.c
@@ -5,17 +5,32 @@
 
 int main(int argc, char *argv[]) {
 	
+	printf("Hello Sookmyung EE!\n");
+
+	
 	int x;
 	char y;
 	short z;
-	float m;
-	double n;
+	float p;
+	double q;
 	
 	printf("bytes of int : %d\n", sizeof(x));
 	printf("bytes of char : %d\n", sizeof(y));
 	printf("bytes of short : %d\n", sizeof(z));
-	printf("bytes of float : %d\n", sizeof(m));
-	printf("bytes of double : %d\n", sizeof(n));
+	printf("bytes of float : %d\n", sizeof(p));
+	printf("bytes of double : %d\n", sizeof(q));
+	
+	
+	int input_int;
+	float input_float;
+	
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
+	
+	printf("enter a float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
 	
 	return 0;
 }
